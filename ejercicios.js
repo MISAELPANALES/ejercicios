a83eec0c30f5db7635921f941838d968cca85e7d
MISAELPANALES <misatalonia@gmail.com>
2023-05-30 15:51:15 -0600

    ejercicios

---
+++ ejercicios.js
@@ -0,0 +1,33 @@
+//1. Calcula los impuestos
+//Escribe una función llamada calcularImpuestos que reciba dos argumentos numéricos: 
+//edad e ingresos. Si edad es igual o mayor a 18 y los ingresos 
+//son iguales o mayores a 1000 debe retornar ingresos * 40%. De lo contrario retorna 0.
+//Ejemplos:
+
+calcularImpuestos(18, 1000) // retorna 400
+calcularImpuestos(40, 10000) // retorna 4000
+calcularImpuestos(17, 5000) // retorna 0
+
+//codigo:
+
+
+//2. Cuenta los asteriscos
+//Escribe una función llamada asteriscos 
+//que reciba un string str y retorne el
+//número de asteriscos que hay en str.
+//Ejemplos:
+//asteriscos("H*o*l*a") // retorna 3
+//asteriscos("Hola")  // retorna 0
+//codigo:
+
+
+
+
+//. Duplica cada element
+//Escribe una función llamada duplicar que reciba un arreglo 
+//de números como parámetro y retorne un nuevo arreglo 
+//con cada elemento duplicado (multiplicado por dos).
+//Ejemplos:
+
+//duplicar([3, 12, 45, 7]) // retorna [6, 24, 90, 14]
+//duplicar([8, 5]) // retorna [16, 10]
