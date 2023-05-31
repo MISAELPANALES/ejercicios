08dcdd19d351f6b31ee1820eb2f901cfa0faa2f9
MISAELPANALES <misatalonia@gmail.com>
2023-05-30 22:27:43 -0600

    actualizacion

--- ejercicios.js
+++ ejercicios.js
@@ -9,6 +9,13 @@ calcularImpuestos(40, 10000) // retorna 4000
 calcularImpuestos(17, 5000) // retorna 0
 
 //codigo:
+function calcularImpuestos(edad, ingresos){
+    if((edad >= 18) && (ingresos >= 1000)){
+        return ingresos * 0.40
+    }else{
+        return 0
+    }
+}
 
 
 //2. Cuenta los asteriscos
@@ -19,8 +26,16 @@ calcularImpuestos(17, 5000) // retorna 0
 //asteriscos("H*o*l*a") // retorna 3
 //asteriscos("Hola")  // retorna 0
 //codigo:
-
-
+function asteriscos(str){
+    let asterisco = "*";
+    let contador = 0;
+    for(i = 0; i < str.length; ++i){
+        if(asterisco.indexOf(str[i]) !== -1){
+            ++contador
+        }
+    }
+    return contador;
+}
 
 
 //. Duplica cada element
@@ -28,6 +43,11 @@ calcularImpuestos(17, 5000) // retorna 0
 //de números como parámetro y retorne un nuevo arreglo 
 //con cada elemento duplicado (multiplicado por dos).
 //Ejemplos:
+const duplicar = function (array){
+    return array.map(function(element){
+        return element * 2
+    })
+}
 
 //duplicar([3, 12, 45, 7]) // retorna [6, 24, 90, 14]
 //duplicar([8, 5]) // retorna [16, 10]
